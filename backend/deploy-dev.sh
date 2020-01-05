0dc48c51bcead92ba87d2d7211d3e120d8339f52
Kemgne Moyo Steeve Aymard <kemgnemoyosteeveaymard@gmail.com>
2020-01-05 17:54:20 +0100

    add travis ci/cd

---
+++ backend/deploy-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Install Dependencies"
+npm ci
+
+echo "Set Max Garbage Heap of Node to 1024"
+export NODE_OPTIONS=--max-old-space-size=${MAX_OLD_SPACE_SIZE}
+
+echo "Deploy Application In Dev Stage"
+serverless deploy -v -s dev 
