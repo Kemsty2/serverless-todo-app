9e69e7363ef05e80fbb594800f1e888f8a37ac97
Kemsty2 <32348054+Kemsty2@users.noreply.github.com>
2020-01-06 17:26:18 +0100

    Add files via upload

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -7,12 +7,18 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { getJwtToken } from '../utils'
 
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getTodo')
+
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   const jwtToken = getJwtToken(event)
+  logger.info('Get jwt Token', jwtToken)
   
   const items = await getAllTodos(jwtToken);
-
+  logger.info('Get all the todo of the current user', items)
+  
   return {
     statusCode: 200,
     body: JSON.stringify({items})
