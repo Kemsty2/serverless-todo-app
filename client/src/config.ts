3c94d907d4520be83521395ae3c6d422ef7479e3
Kemgne Moyo Steeve Aymard <kemgnemoyosteeveaymard@gmail.com>
2020-01-06 22:56:14 +0100

    Fix bugs

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 's6t29sgwc5'
+const apiId = 'j5shq7w5g4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
