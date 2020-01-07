858a65310a2b4e336b279899e2eefaa0db5c5cfd
Kemsty2 <32348054+Kemsty2@users.noreply.github.com>
2020-01-07 08:23:39 +0100

    Update createTodo.ts

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -37,7 +37,7 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
   return {
     statusCode: 201,    
     body: JSON.stringify({
-      newItem
+      item: newItem
     })
   }
 })
