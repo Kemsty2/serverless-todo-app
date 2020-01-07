2a8d54a71e8a26efbdeead42ebdf8d0f1450d5ab
Kemgne Moyo Steeve Aymard <kemgnemoyosteeveaymard@gmail.com>
2020-01-07 01:24:37 +0100

    Add apiId

--- backend/deploy-prod.sh
+++ backend/deploy-prod.sh
@@ -5,10 +5,10 @@ set -ev
 cd "$PWD"/backend
 
 echo "Install Dependencies"
-npm ci
+#npm ci
 
 echo "Set Max Garbage Heap of Node to 1024"
-export NODE_OPTIONS=--max-old-space-size=${MAX_OLD_SPACE_SIZE}
+#export NODE_OPTIONS=--max-old-space-size=${MAX_OLD_SPACE_SIZE}
 
 echo "Deploy Application In Dev Stage"
-serverless deploy -v -s prod 
+#serverless deploy -v -s prod 
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '35u5tt3qd8'
+const apiId = 'l0u8a78gik'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
